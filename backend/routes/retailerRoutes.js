2c5643690612464b9f8becb62e1bf6745228f991
Pratham Jain <2muchappy420@gmail.com>
2025-12-20 15:40:17 +0530

    route file fixed

--- backend/routes/retailerRoutes.js
+++ backend/routes/retailerRoutes.js
@@ -1,17 +1,17 @@
 import express from "express";
 import {
-  sendOtp,
-  verifyOtp,
-  registerRetailer,
-  loginRetailer,
-  getRetailerProfile,
-  getRetailerCampaigns,
-  updateCampaignStatus,
+    sendOtp,
+    verifyOtp,
+    registerRetailer,
+    loginRetailer,
+    getRetailerProfile,
+    getRetailerCampaigns,
+    updateCampaignStatus,
     updateRetailer,
     submitRetailerReport,
     getRetailerReports,
-     viewBillCopy,
-       viewReportImage
+    viewBillCopy,
+    viewReportImage,
 } from "../controllers/retailerController.js";
 import multer from "multer";
 import { protect } from "../middleware/authMiddleware.js"; // JWT middleware
@@ -20,11 +20,10 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 const uploadFiles = upload.fields([
-  { name: "govtIdPhoto", maxCount: 1 },
-  { name: "personPhoto", maxCount: 1 },
-  { name: "signature", maxCount: 1 },
-  { name: "outletPhoto", maxCount: 1 },
-  { name: "registrationForm", maxCount: 1 },
+    { name: "govtIdPhoto", maxCount: 1 },
+    { name: "personPhoto", maxCount: 1 },
+    { name: "outletPhoto", maxCount: 1 },
+    { name: "registrationFormFile", maxCount: 1 },
 ]);
 
 const router = express.Router();
@@ -38,7 +37,7 @@ router.post("/login", loginRetailer);
 
 // -------------------------------
 // PROTECTED ROUTES
-router.get("/retailer/me",protect, getRetailerProfile);
+router.get("/retailer/me", protect, getRetailerProfile);
 
 // Fetch only campaigns assigned to this retailer
 router.get("/campaigns", protect, getRetailerCampaigns);
@@ -46,37 +45,29 @@ router.get("/campaigns", protect, getRetailerCampaigns);
 //  Retailer accepts or rejects a campaign
 router.put("/campaigns/:campaignId/status", protect, updateCampaignStatus);
 router.patch(
-  "/me",
-  protect,
-  upload.fields([
-    { name: "govtIdPhoto", maxCount: 1 },
-    { name: "personPhoto", maxCount: 1 },
-    { name: "signature", maxCount: 1 },
-    { name: "outletPhoto", maxCount: 1 }
-  ]),
-  updateRetailer
+    "/me",
+    protect,
+    upload.fields([
+        { name: "govtIdPhoto", maxCount: 1 },
+        { name: "personPhoto", maxCount: 1 },
+        { name: "registrationFormFile", maxCount: 1 },
+        { name: "outletPhoto", maxCount: 1 },
+    ]),
+    updateRetailer
 );
 router.post(
-  "/retailer/report/submit",
-  protect, // Retailer auth
-  upload.fields([
-    { name: "images", maxCount: 10 },
-    { name: "billCopy", maxCount: 1 } // OPTIONAL if you also want bill copy
-  ]),
-  submitRetailerReport
+    "/retailer/report/submit",
+    protect, // Retailer auth
+    upload.fields([
+        { name: "images", maxCount: 10 },
+        { name: "billCopy", maxCount: 1 }, // OPTIONAL if you also want bill copy
+    ]),
+    submitRetailerReport
 );
 
 router.get("/retailer/reports", protect, getRetailerReports);
-router.get(
-  "/reports/:reportId/images/:imageIndex",
-  protect,
-  viewReportImage
-);
+router.get("/reports/:reportId/images/:imageIndex", protect, viewReportImage);
 
-router.get(
-  "/reports/:reportId/bill",
-  protect,
-  viewBillCopy
-);
+router.get("/reports/:reportId/bill", protect, viewBillCopy);
 
 export default router;
